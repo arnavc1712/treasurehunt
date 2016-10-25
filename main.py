4f7e66e2c74f47357d859ff3987851756f1a25ba
arnavc1712 <arnav171296@gmail.com>
2016-10-25 20:20:38 +0530

    Added Page Ranking Algorithm
    Also included robot.txt file reading

--- main.py
+++ main.py
@@ -14,17 +14,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 import webapp2
-import urllib
+import urllib2
 import sys
 import os
 import jinja2
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 
 sys.path.insert(0, 'libs')
 from bs4 import BeautifulSoup
 from stop_words import get_stop_words
 from urlparse import urlparse, urljoin
 from google.appengine.api import urlfetch
+import robotexclusionrulesparser as rerp
 urlfetch.set_default_fetch_deadline(45)
 
 
@@ -42,21 +62,59 @@ stop_words = get_stop_words('english')
 
 
 def get_page(page):
-	return BeautifulSoup(urllib.urlopen(page).read())
+	page_url = urlparse(page)
+	if page_url[0]:
+		base = page_url[0] + '://' + page_url[1]
+		robots_url=urljoin(base,'/robots.txt')
+	else:
+		base= "https" + page_url[2]
+		robots_url=urljoin(base,'/robots.txt')
+	rp=rerp.RobotFileParserLookalike()
+	rp.set_url(robots_url)
+	try:
+		rp.read()
+	except:
+		pass
+	if not rp.can_fetch('*', page):
+		print "[get_page()] Page off limits!"
+		return BeautifulSoup(""), ""
+
+	if page in cache:
+		return cache[page],page
+	else:
+		try:
+			return BeautifulSoup(urllib2.urlopen(page).read()),page
+		except:
+			return BeautifulSoup(""),""
+
+	
 
 def get_all_links(page,url):     #GETTING LINKS PRESENT IN EACH CRAWLED PAGE
 	links=[]
 	urls=[]
 	page_url=urlparse(url)
+	try:
+		page_data=urllib2.urlopen(url)
+	except:
+		pass
+	# if page_data.getheader('Content-Type')=="text/html":
 	if page_url[0]:
 		base = page_url[0] + '://' + page_url[1]
+		robots_url=urljoin(base,'/robots.txt')
 	else:
-		base= "https://" + page_url[1]
+		base= "https" + page_url[2]
+		robots_url=urljoin(base,'/robots.txt')
+	rp=rerp.RobotFileParserLookalike()
+	try:
+		rp.read()
+	except:
+		pass
 	# soup=BeautifulSoup(page,'html.parser')
 	for link in page.find_all('a'):
 	 	link_url=link.get('href')
+	 	
 	 	print "[get_all_links()] Found a link: ", link_url
-	 	if link_url == None or "javascript" in link_url or "mailto" in link_url or "tel" in link_url: 
+	 	if link_url == None or "javascript" in link_url or "mailto" in link_url or "tel" in link_url or link_url=="": 
 			pass
 		elif urlparse(link_url)[5] and not urlparse(link_url)[2]: 
 			pass
@@ -70,26 +128,39 @@ def get_all_links(page,url):     #GETTING LINKS PRESENT IN EACH CRAWLED PAGE
 def union(p,q):   
 	for e in q:
 		if e not in p:
-			p.append(e)
+			if is_youtube(e):
+				p.append(e)
 
-def crawl_web(seed):    #CRAWLING ALL WEB PAGES FROM A GIVEN SEED PAGE
-	tocrawl=[seed]
+def crawl_web(seed):   #CRAWLING ALL WEB PAGES FROM A GIVEN SEED PAGE
+	tocrawl=[]  
+	for urls in seed:
+		if is_youtube(urls):
+			tocrawl.append(urls)
+	print tocrawl
+
 	crawled=[]
 	i=0
 	while tocrawl:
 		page=tocrawl.pop()
-		if page not in crawled and i<=2:
-			content=get_page(page)
+		if page not in crawled and len(crawled)<100:
+			
+			content,page=get_page(page)
+			cache[page] = content
 			add_page_to_index(index,page,content)
-			union(tocrawl,get_all_links(content,page))
+			outlinks=get_all_links(content,page)
+			graph[page]=outlinks
+			union(tocrawl,outlinks)
 			crawled.append(page)
-			i=i+1
+			# i=i+1
+			
 			
 
 
 	# return index
 
 index = {}
+graph={}
+cache={}
 
 
 def add_to_index(index,keyword,url):  #CREATING AN INDEX OF KEYWORDS AND THE URL'S THEY ARE LINKED TO
@@ -112,7 +183,7 @@ def add_to_index(index,keyword,url):  #CREATING AN INDEX OF KEYWORDS AND THE URL
 def add_page_to_index(index,url,content): #SPLITTING EACH WORD PRESENT IN THE TEXT AND REMOVING STOP WORDS AND PUNCTUATIONS
     words=[]
     try:
-        text = content.body.get_text()
+        text = content.body.get_text().encode('utf-8')
     except AttributeError:
         return
    
@@ -131,6 +202,29 @@ def add_page_to_index(index,url,content): #SPLITTING EACH WORD PRESENT IN THE TE
 
 
 
+
+def compute_ranks(graph):
+    d = 0.8 # damping factor
+    numloops = 20
+    
+    ranks = {}
+    npages = len(graph)
+    for page in graph:
+        ranks[page] = 1.0 / npages
+    
+    for i in range(0, numloops):
+        newranks = {}
+        for page in graph:
+            newrank = (1 - d) / npages
+            for nodes in graph:
+                if page in graph[nodes]:
+                    newrank=newrank + (d*ranks[nodes])/len(graph[nodes])
+            newranks[page] = newrank
+        ranks = newranks
+    return ranks
+
+
+
 def lookup(index,keyword): #LOOKING UP THE INDEX TO FIND THE WORDS QUERIED
     urls=[]
     words=[]
@@ -144,7 +238,16 @@ def lookup(index,keyword): #LOOKING UP THE INDEX TO FIND THE WORDS QUERIED
     		for url in index[word]:
     			if url not in urls:
     				urls.append(url)
-    return urls        
+    return urls    
+
+
+
+def is_youtube(page):
+	parsed_url=urlparse(page)
+	if parsed_url[1]=="in.news.yahoo.com":
+		return True
+	else:
+		False
 
 
 
@@ -176,12 +279,13 @@ def lookup(index,keyword): #LOOKING UP THE INDEX TO FIND THE WORDS QUERIED
 #             break
 #     return links	
 
-def union_string(p,q):
-	for e in q:
-		if e not in p:
-			p=p+q
-	q=p	
-	return q	
+# def union_string(p,q):
+# 	for e in q:
+# 		if e not in p:
+# 			if is_youtube(e):
+# 				p=p+q
+# 	q=p	
+# 	return q	
 
 		
 			
@@ -219,14 +323,19 @@ class Handler(webapp2.RequestHandler):
 	def render(self,template,**kw):
 		self.write(self.render_str(template,**kw))
 
+
+seed=["http://in.news.yahoo.com/"]
+crawl_web(seed)
+
 class MainHandler(Handler):
     def get(self):
-    	crawl_web("http://www.udacity.com/overview/Course/cs101/")
-    	self.render("front-page.html")
-    # 	self.write(index)
+    	
+    	# self.render("front-page.html")
+    	self.write(compute_ranks(graph))
     	
 
     def post(self):
+    	
     	query=self.request.get("query")
     	urls=[]
     	global index
